2a488b44ca765cb9b80dd50d26d1b439de42dbb0
Ron <80211681+rongplus@users.noreply.github.com>
2024-10-23 00:01:02 +0000

    N1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit as st
-
+st.set_page_config(        page_title="My Page Title",)
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+with st.sidebar:
+    st.page_link("pages/p2.py", label="Page 2" , icon="🧪")
+    st.page_link("pages/p1.py", label="Page 3", icon="🌎")
